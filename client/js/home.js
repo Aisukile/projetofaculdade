fd6b847f613f0c027858ce1fc0ae441bc29c95f7
Aisukile <105235601+Aisukile@users.noreply.github.com>
2023-12-18 10:48:25 -0300

    Add files via upload

---
+++ client/js/home.js
@@ -0,0 +1,15 @@
+function redirecionaCadastro(){
+    window.location.href = "cadastraLivro.html";
+}
+
+function redirecionaConsulta(){
+    window.location.href = "/consultaLivro";
+}
+
+function redirecionaDelete(){
+    window.location.href = "/deleteLivro";
+}
+
+function redirecionaAtualiza(){
+    window.location.href = "/atualizaLivro";
+}
